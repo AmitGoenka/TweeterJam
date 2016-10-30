f8d118a47a54ab11d27ffdb2a54964ac5b3ee1cd
Amit Goenka <master.goenka@gmail.com>
2016-10-30 18:16:38 -0500

    Mostly image updates

--- app/src/main/java/org/agoenka/tweeterjam/activities/DetailActivity.java
+++ app/src/main/java/org/agoenka/tweeterjam/activities/DetailActivity.java
@@ -3,6 +3,7 @@ package org.agoenka.tweeterjam.activities;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import org.agoenka.tweeterjam.R;
 import org.agoenka.tweeterjam.databinding.ActivityDetailBinding;
@@ -19,8 +20,15 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+        binding.setHandlers(new Handlers());
 
         Tweet tweet = Parcels.unwrap(getIntent().getParcelableExtra(TWEET_KEY));
         binding.setTweet(tweet);
+    }
+
+    public class Handlers {
+        public void onReply(@SuppressWarnings("unused") View view) {
+
+        }
     }
 }
