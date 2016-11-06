f53fdc886e02d6558119ec2ebfed0b63835c381f
Amit Goenka <master.goenka@gmail.com>
2016-11-06 12:54:23 -0600

    Minor Refactoring

--- app/src/main/java/org/agoenka/tweeterjam/activities/ProfileActivity.java
+++ app/src/main/java/org/agoenka/tweeterjam/activities/ProfileActivity.java
@@ -3,7 +3,6 @@ package org.agoenka.tweeterjam.activities;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import org.agoenka.tweeterjam.R;
@@ -29,24 +28,32 @@ public class ProfileActivity extends AppCompatActivity implements TweetsListFrag
 
         loggedInUser = Parcels.unwrap(getIntent().getParcelableExtra(KEY_LOGGED_IN_USER));
         user = Parcels.unwrap(getIntent().getParcelableExtra(KEY_USER));
+        bindViews();
+
+        if (savedInstanceState == null) {
+            loadUserTimeline();
+        }
+    }
+
+    private void loadUserTimeline() {
+        // Create the user timeline fragment
+        UserTimelineFragment timelineFragment = UserTimelineFragment.newInstance(user.getScreenName());
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flContainer, timelineFragment)
+                .commit();
+    }
+
+    private void bindViews() {
+        ActivityProfileBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+
         if (user == null)
             user = loggedInUser;
-
-        ActivityProfileBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
         binding.setUser(user);
 
         setSupportActionBar(binding.appbarMain.toolbar);
         assert getSupportActionBar() != null;
         getSupportActionBar().setTitle(user.getScreenName());
-
-        if (savedInstanceState == null) {
-            // Create the user timeline fragment
-            UserTimelineFragment userTimelineFragment = UserTimelineFragment.newInstance(user.getScreenName());
-            // Display user fragment within this activity(dynamically)
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.flContainer, userTimelineFragment);
-            ft.commit();// changes the fragments
-        }
     }
 
     @Override
